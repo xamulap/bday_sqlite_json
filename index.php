8168c8e390f269fce3678615d0aac7478e4a4770
Peter Chamula <peter.chamula@darksun.sk>
2016-06-23 02:26:31 +0200

    range fix

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ get bday for next month
 $dir = 'sqlite:db/bdays';
 $dbh = new PDO($dir) or die("cannot open database");
 $month = date("m-d", strtotime("+1 month", mktime()));
-$query = "SELECT name,substr(date,6) as date FROM bdays where substr(date,6)>='".$month."' ";
+$now = date("m-d", mktime());
+$query = "SELECT name,substr(date,6) as date FROM bdays where substr(date,6)<='".$month."' and substr(date,6)>'".$now."'";
 foreach ($dbh->query($query) as $row) {
-	echo json_encode($row);
+	$rows[]=$row;
 }
+echo json_encode($rows);
 ?>
 
